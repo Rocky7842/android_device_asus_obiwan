a5068fc4a9c4d2ef256788edbd07da5b5a02045c
Bruno Martins <bgcngm@gmail.com>
2024-05-13 12:53:20 -0400

    obiwan: Migrate to QTI health AIDL service
    The health AIDL HAL service provides functionalities of charger,
    therefore system charger at /system/bin/charger is deprecated.
    On top of that, QTI health AIDL HAL service enables suspend by
    default, the equivalent of setting ro.charger.enable_suspend
    for legacy charger.
    
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- device.mk
+++ device.mk
@@ -216,8 +216,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti
 
 # Hotword Enrollment
 PRODUCT_COPY_FILES += \
