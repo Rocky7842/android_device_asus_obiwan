d5d20848e826ef35fcce1ae46ffad0d8c5eea60a
mikeNG <mkbestas@lineageos.org>
2024-06-10 15:57:19 +0800

    obiwan: AsusParts: Update DozePreferenceFragment for QPR2
    The OnMainSwitchChangeListener is replaced with OnCheckedChangeListener

--- AsusParts/src/org/lineageos/settings/asusparts/doze/DozeSettingsFragment.java
+++ AsusParts/src/org/lineageos/settings/asusparts/doze/DozeSettingsFragment.java
@@ -25,19 +25,19 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Switch;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.settingslib.widget.MainSwitchPreference;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
 import org.lineageos.settings.asusparts.R;
 
 public class DozeSettingsFragment extends PreferenceFragment implements 
-        Preference.OnPreferenceChangeListener, OnMainSwitchChangeListener {
+        Preference.OnPreferenceChangeListener, OnCheckedChangeListener {
 
     private MainSwitchPreference mSwitchBar;
 
@@ -112,7 +112,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements
     }
 
     @Override
-    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         DozeUtils.enableDoze(getActivity(), isChecked);
         DozeUtils.checkDozeService(getActivity());
 
