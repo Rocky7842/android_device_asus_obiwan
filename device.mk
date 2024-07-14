448b815bdab4231b2647b4fedca9ef52f5863354
Rocky7842 <eric.rocky7842@gmail.com>
2024-07-14 22:24:23 +0800

    obiwan: Implement sensor sub HAL
    * Imported from sake.
    * Keep only needed udfps sensor and adapt paths/values for obiwan.
    
    Change-Id: Ia932e6d5656ca44646b0aebb12cba2c22a57a644

--- device.mk
+++ device.mk
@@ -393,6 +393,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 PRODUCT_PACKAGES += \
+    sensors.obiwan
+
+PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
